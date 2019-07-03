322764eac0ff2e03b4ce630b4d21d17ef28e76a4
Artem <mstrJesm@gmail.com>
2019-07-03 11:41:34 +0300

    my comment

---
+++ src/Pizza.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>Pizza name {{ pizzaName }}</h2>
+        <p>Price: {{ pizzaPrice }}</p>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props: ['pizzaName', 'pizzaPrice'],
+}
+</script>
